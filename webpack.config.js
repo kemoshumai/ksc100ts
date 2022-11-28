9d9439abc1ea5610e353597a06c6b870fac11e05
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2022-11-29 01:46:39 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/main.ts',
+    target: 'node',
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: ['ts-loader'],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ExternalsPlugin('commonjs', [
+            'llvm-bindings' 
+        ]) 
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    }
+};
